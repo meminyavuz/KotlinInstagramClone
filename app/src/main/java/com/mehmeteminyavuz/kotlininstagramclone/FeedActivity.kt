36d77afcf0bcdd11125a16c3b55235063a31e025
Mehmet Emin Yavuz <yavuz.m.e@outlook.com>
2023-08-20 17:06:37 +0300

    app permissions done

--- app/src/main/java/com/mehmeteminyavuz/kotlininstagramclone/FeedActivity.kt
+++ app/src/main/java/com/mehmeteminyavuz/kotlininstagramclone/FeedActivity.kt
@@ -1,27 +1,83 @@
 package com.mehmeteminyavuz.kotlininstagramclone
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import com.google.android.material.snackbar.Snackbar
 import com.mehmeteminyavuz.kotlininstagramclone.databinding.ActivityFeedBinding
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedImage: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        registerLauncher()
     }
 
     fun addImageClicked(view:View){
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_MEDIA_IMAGES)){
+                Snackbar.make(view,"Permission needed for gallery!",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                }.show()
+            }
+            else{
+                permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
 
+            }
+        }
+        else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+        }
     }
 
     fun sendClicked(view: View){
 
     }
 
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if (result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if (intentFromResult != null){
+                    selectedImage = intentFromResult.data
+                    selectedImage?.let {
+                        binding.addImage.setImageURI(it)
+                    }
+                }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result ->
+
+            if (result){
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+            else {
+                //permission denied
+                Toast.makeText(this@FeedActivity,"Permission needed!",Toast.LENGTH_LONG)
+            }
+
+        }
+    }
 
 }
